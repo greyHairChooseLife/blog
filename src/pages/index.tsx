fbf68b517597f3788a0c3a6138f16b7e9a451269
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-14 18:55:31 +0900

    feat: StaticImage component 연습
    - 로컬 파일 시스템에서 이미지 갖다 쓰기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 import Layout from '../components/Layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This page is for 'home page'.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/HABIT_TRACKER_demo.png"
+      />
     </Layout>
   );
 };
