610ca3fd1209fd2f9ccc159de31489cc7177f860
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-15 05:11:40 +0900

    feat: Blog 컴포넌트가 local file system 데이터 사용
    - graphQL data layer에서 가져온 local file system 데이터 활용

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
+import { graphql, PageProps } from 'gatsby';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
-const BlogPage = () => {
+const BlogPage = ({ data }: PageProps<Queries.BlogPageQuery>) => {
+  const posts = data.allFile.nodes;
+
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+        {posts.map(({ name: fileName }) => (
+          <li key={fileName}>{fileName}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
@@ -13,3 +20,13 @@ const BlogPage = () => {
 export const Head = () => <Seo title="My Blog Posts" />;
 
 export default BlogPage;
+
+export const query = graphql`
+  query BlogPage {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
