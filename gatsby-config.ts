8d98be6bae4bc060e91d17d612ef7f5d206624a3
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-15 05:10:20 +0900

    chore: config plugin for local filesystem

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,8 +9,18 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ['gatsby-plugin-dts-css-modules', 'gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-dts-css-modules',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
+  ],
 };
 
 export default config;
-
