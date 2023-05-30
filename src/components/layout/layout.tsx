69cd945d2c16e0822b1bb479ffa0c93333cf50f4
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-30 14:27:24 +0900

    style: 블로그 title, navigation 부분에 배경색 처리

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,9 +5,11 @@ import * as st from './layout.module.css';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={st.layout}>
-      <h1>CHOOSE LIFE</h1>
-      <Navigation />
-      {children}
+      <header>
+        <h1>CHOOSE LIFE</h1>
+        <Navigation />
+      </header>
+      <article>{children}</article>
     </div>
   );
 };
