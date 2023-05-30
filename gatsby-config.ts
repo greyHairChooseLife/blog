8f2306e7a5f78ed66a3a84213bc2854a10863a74
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-30 17:27:30 +0900

    feat: gatsby pathPrefix 추가
    - 기본적으로 gatsby의 <Link to='url'> 같은 것은 'root/' 기준으로
      동작한다. 나는 github pages를 이용하는 만큼 주소에 repository title이
      base path가 되는데, 이런 부분을 설정 해 주는 것.
    
      https://www.gatsbyjs.com/docs/how-to/previews-deploys-hosting/path-prefix/

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `choose life`,
     siteUrl: `https://greyhairchooselife.github.io/blog/`,
   },
+  pathPrefix: '/blog',
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
