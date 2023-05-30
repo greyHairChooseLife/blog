c73630ad95413dc28cc629760b059cbc24ca1ca0
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-30 16:25:24 +0900

    chore: install & config gh-pages
    - `npm install gh-pages --save-dev`
    - `npm run deploy` script
      1. 배포용 브랜치인 main 브랜치로 checkout
      2. gatsby build
      3. gh-pages 활용하여 github pages로 배포
      4. gh-pages에서 -b 옵션으로 지정하는 브랜치는 remote repo의 어느
         브랜치에서 배포 할 것인지를 뜻한다.
    - site url 변경(github page url)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `choose life`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://greyhairchooselife.github.io/blog/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
